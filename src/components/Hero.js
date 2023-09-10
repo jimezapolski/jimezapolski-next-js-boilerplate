173b84de10baab18407db82814df43d63cef16be
jimezapolski <jimezapolski@gmail.com>
2023-09-10 13:31:36 -0300

    change cover and navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,10 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div
-      className={`hero flex justify-center items-center w-full h-[600px] bg-[url('/assets/hero.png')] bg-cover bg-top mb-6`}
+      className={`hero flex justify-center items-center w-full h-[600px] bg-[url('/assets/hero.jpeg')] bg-cover bg-top mb-6`}
     >
       <div className=" bg-[rgba(0,0,0,.6)] w-full h-full flex justify-center items-center">
-        <h1 className="text-xl sm:text-7xl">Welcome to the machine</h1>
+        {/* <h1 className=" text-white text-xl md:text-7xl sm:text-7xl">Welcome to the machine</h1> */}
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,20 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 const Navbar = () => {
 
   return (
-   <div className='flex justify-between items-center px-8 h-14 bg-[#FDF104]'>
+   <div className='flex justify-between items-center px-8 h-14 bg-[#000000] shadow-xl shadow-cyan-500/50 '>
     <div className='h-auto w-16 sm:w-24 md:w-20'>
-      <Image src={`/assets/batman-logo.png`} width={80} height={60}  alt={`logo`} ></Image>
+      <Image src={`/assets/Logo.png`} width={160} height={80}  alt={`logo`} ></Image>
     </div>
     <nav className='hidden sm:block'>
-        <ul className='flex justify-end items-center gap-4 text-black'>
-          <li>About</li>
-          <li>Contact</li>
+        <ul className='flex justify-end items-center gap-4 text-white opacity-75'>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Trending</li>
+          <li>Categories</li>
         </ul>
       </nav>
       <div className='sm:hidden'>
-        <RxHamburgerMenu size={20} color={`#000000`} />
+        <RxHamburgerMenu size={20} color={`#FFFFFF`} />
       </div>
 
     </div>
